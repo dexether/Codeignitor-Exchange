a362179e6121b35860075577c44dc690e2a10b8c
Mohamed Mahdy <php.power.arts@gmail.com>
2017-03-09 18:06:46 +0200

    add email layout

---
+++ application/views/template/emails/footer.php
@@ -0,0 +1,22 @@
+ <p style="margin: 0pt 20px; padding: 0px; text-align: left; color: rgb(84, 84, 84); font-family: Helvetica,Arial,sans-serif; font-size: 14px;">Best Regards,</p>
+
+ <p style="margin: 0pt 20px; padding: 15px 0px; text-align: left; color: rgb(84, 84, 84); font-family: Helvetica,Arial,sans-serif; font-size: 14px;">##COMPANYNAME##</p>
+</td>
+</tr>
+<tr>
+	<td style="padding: 20px 30px 5px;"> </td>
+</tr>
+</tbody>
+</table>
+</div>
+
+<table cellpadding="0" cellspacing="0" width="720px">
+	<tbody>
+		<tr>
+			<td align="center"> </td>
+		</tr>
+	</tbody>
+</table>
+
+<p> </p>
+</div>
