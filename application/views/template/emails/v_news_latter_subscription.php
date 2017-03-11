99369439b4f4c14487cf008f7321d5506370c422
Mohamed Mahdy <php.power.arts@gmail.com>
2017-03-11 08:20:41 +0200

    add news latter subscription & reset password

---
+++ application/views/template/emails/v_news_latter_subscription.php
@@ -0,0 +1,13 @@
+<table style="background-color: rgb(255, 255, 255);" width="100%">
+ <tbody>
+   <tr>
+      <td style="padding: 20px 30px 5px;">
+      <h2 style="color: rgb(84, 84, 84); margin: 0pt 20px 5px; font-family: Helvetica,Arial,sans-serif; font-size: 18px;"><span style="font-size:16px;"><span style="color:#000000;"><span style="font-family:times new roman,times,serif;">Hello Friend,</span></span></span></h2>
+
+     <h2 style="color: rgb(84, 84, 84); margin: 0pt 20px 5px; font-family: Helvetica, Arial, sans-serif; font-size: 15px; text-align: center;"><span style="font-size:16px;"><span style="color:#000000;"><span style="font-family:times new roman,times,serif;"><span style="line-height: 21.993999481201172px;">Thank you for subscribing to the&nbsp;</span></span></span></span><span style="color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none;">##COMPANYNAME##</span><span style="font-size:16px;"><span style="color:#000000;"><span style="font-family:times new roman,times,serif;"><span style="line-height: 21.993999481201172px;"> newsletter!</span></span></span></span></h2>
+
+     <p style="font-family: Helvetica,Arial,sans-serif; color: rgb(84, 84, 84); margin: 0pt 20px; font-size: 12px; text-align: left; padding: 15px 0px;"><span style="font-family:times new roman,times,serif;"><strong>Regards,&nbsp;</strong></span><span style="color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none;">##COMPANYNAME##</span><span style="font-family:times new roman,times,serif;"><strong> Team.</strong></span></p>
+      </td>
+   </tr>
+ </tbody>
+</table>
