593443a6c3f2cf9d927b8779b0159fd8c464358f
Mohamed Mahdy <php.power.arts@gmail.com>
2017-03-11 08:24:22 +0200

    add success authorization

---
+++ application/views/template/emails/v_success_authorization.php
@@ -0,0 +1,17 @@
+<table style="background-color: rgb(255, 255, 255);" width="100%">
+  <tbody>
+   <tr>
+      <td style="padding: 20px 30px 5px;">
+      <h2 style="color: rgb(84, 84, 84); margin: 0pt 20px 5px; font-family: Helvetica,Arial,sans-serif; font-size: 18px;">Successful authorization.</h2>
+      <br />
+      <h2 style="color: rgb(84, 84, 84); margin: 0pt 20px 5px; font-family: Helvetica,Arial,sans-serif; font-size: 15px;">Login&nbsp; : ##USERNAME##</h2>
+     <br />
+      <h2 style="color: rgb(84, 84, 84); margin: 0pt 20px 5px; font-family: Helvetica,Arial,sans-serif; font-size: 15px;">IP&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : ##IPADDRESS##</h2>
+     <br />
+      <h2 style="color: rgb(84, 84, 84); margin: 0pt 20px 5px; font-family: Helvetica,Arial,sans-serif; font-size: 15px;">Time&nbsp;&nbsp; : ##DATETIME##</h2>
+
+      <p style="font-family: Helvetica,Arial,sans-serif; color: rgb(84, 84, 84); margin: 0pt 20px; font-size: 12px; text-align: left; padding: 15px 0px;">Regards, <span style="color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none;">##COMPANYNAME##</span> Team.</p>
+     </td>
+   </tr>
+ </tbody>
+</table>
