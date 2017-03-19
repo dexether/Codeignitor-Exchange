104d9480e173884834028e41abcae493810d6a0f
Mohamed Mahdy <php.power.arts@gmail.com>
2017-03-19 12:10:29 +0200

    update email templates

--- application/views/template/emails/v_header.php
+++ application/views/template/emails/v_header.php
@@ -2,14 +2,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	
+
 	<meta name="viewport" content="width=device-width"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<title>ZURBemails</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo site_url('css/email.css') ?>"/>
 </head>
 <body bgcolor="#FFFFFF">
-	
+
 	<table class="head-wrap" bgcolor="#999999">
 		<tr>
 			<td></td>
@@ -17,8 +17,8 @@
 				<div class="content">
 					<table bgcolor="#999999">
 						<tr>
-							<td><img src="https://placehold.it/200x50/"/></td>
-							<td align="right"><h6 class="collapse">Basic</h6></td>
+						<td><img src="<?php echo APP_LOGO ?>"/></td>
+							<td align="right"><h6 class="collapse"><?php echo APP_TITLE ?></h6></td>
 						</tr>
 					</table>
 				</div>
@@ -26,7 +26,7 @@
 			<td></td>
 		</tr>
 	</table> 
-	
+
 	<table class="body-wrap">
 		<tr>
 			<td></td>
