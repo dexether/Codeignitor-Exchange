a446396fe0ecd34b544363f700ef80dc94c4ba3d
Mohamed Mahdy <php.power.arts@gmail.com>
2017-04-02 17:47:21 +0200

    add csrf hidden inputs

--- application/views/user/v_change_password.php
+++ application/views/user/v_change_password.php
@@ -11,16 +11,12 @@
 
         <?php $this->load->view('user/v_aboutsidebar'); ?>
         <div class="col-md-9 col-sm-8">
-
           <div class="cls_comm_head"> Change password </div>
-
           <div class="cls_change_pass">
-
-            <form class="form-horizontal" name="password_form" id="password_form" method="post">
+            <?php echo form_open('', 'class="form-horizontal" name="password_form" id="password_form" method="post"'); ?>
               <div class="form-group">
                <span id="password_success" class="alert alert-info" style="display:none;margin:25px;"></span> 
              </div>
-
              <div class="form-group">
               <label for="inputEmail3" class=" col-sm-4  col-sm-offset-2  col-md-3  col-md-offset-2 control-label"> Your Current Password
               </label>
@@ -28,32 +24,25 @@
                 <input type="password" class="form-control" id="oldpassword" name="oldpassword" placeholder="">
               </div>
             </div>
-            
-            
             <div class="form-group">
               <label for="inputEmail3" class=" col-sm-4  col-sm-offset-2  col-md-3  col-md-offset-2 control-label">New Password</label>
               <div class="col-sm-6">
                 <input type="password" class="form-control" id="newpassword" name="newpassword" placeholder="">
               </div>
             </div>
-            
             <div class="form-group">
               <label for="inputEmail3" class=" col-sm-4  col-sm-offset-2  col-md-3  col-md-offset-2 control-label">Confirm New Password</label>
               <div class="col-sm-6">
                 <input type="password" class="form-control" id="newpassword1" name="newpassword1" placeholder="">
               </div>
             </div> 
-            
-            
             <div class="form-group">
               <label for="inputEmail3" class=" col-sm-4  col-sm-offset-2  col-md-3  col-md-offset-2 control-label">&nbsp;</label>
               <div class="col-sm-6">
                <button type="submit" class="cls_dow_btn" id="password_update" name="password_update">  Update Password </button> 
              </div>
            </div>
-
-           
-         </form>
+         <?php echo form_close(); ?>
        </div>
 
      </div>
