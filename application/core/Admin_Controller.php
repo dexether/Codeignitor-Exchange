44c27439b30d804809e8997c0d1bb716466c71ee
Mohamed Mahdy <php.power.arts@gmail.com>
2017-04-02 16:52:39 +0200

    remove .idea folder

---
+++ application/core/Admin_Controller.php
@@ -0,0 +1,28 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/**
+ * Description of base_controller
+ *
+ * @author rogier
+ */
+class Admin_Controller extends CI_Controller
+{
+    /**
+     * base array for storing view strings
+     *
+     * @var array
+     */
+    public $data;
+
+    /**
+     * Contructor, used to reference to the parents constructor.
+     */
+    public function  __construct()
+    {
+        parent::__construct();
+
+        //preset the variables needed in the templates
+        $this->data = base_vars();
+    }
+
+}
