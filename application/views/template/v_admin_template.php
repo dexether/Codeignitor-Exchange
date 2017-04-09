09274438e875c29cbf0ec5891fe1c83a84dfe614
Mohamed Mahdy <php.power.arts@gmail.com>
2017-04-09 18:09:07 +0200

    update admin template

--- application/views/template/v_admin_template.php
+++ application/views/template/v_admin_template.php
@@ -43,9 +43,9 @@
 			<!-- Logo -->
 			<a href="index2.html" class="logo">
 				<!-- mini logo for sidebar mini 50x50 pixels -->
-				<span class="logo-mini"><b>A</b>LT</span>
+				<span class="logo-mini"><b>G</b></span>
 				<!-- logo for regular state and mobile devices -->
-				<span class="logo-lg"><b>Admin</b>LTE</span>
+				<span class="logo-lg"><b>Gulden Trader</b></span>
 			</a>
 			<!-- Header Navbar: style can be found in header.less -->
 			<nav class="navbar navbar-static-top">
@@ -114,8 +114,9 @@
 				<!-- /.search form -->
 				<!-- sidebar menu: : style can be found in sidebar.less -->
 				<ul class="sidebar-menu">
-					<li><a href="<?php echo site_url('admin') ?>"><i class="fa fa-dashboard"></i>Dashboard</a></li>
-					<li><a href="<?php echo site_url('admin/users') ?>"><i class="fa fa-users"></i>Manager Users</a></li>
+					<li><a href="<?php echo site_url() ?>"> Visite Website</a></li>
+					<li><a href="<?php echo site_url('admin') ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					<li><a href="<?php echo site_url('admin/users') ?>"><i class="fa fa-users"></i> Manager Users</a></li>
 				</ul>
 			</section>
 			<!-- /.sidebar -->
