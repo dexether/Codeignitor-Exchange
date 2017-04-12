32486539456993d7cc3190a9f6dc4d5a934f0954
Mohamed Mahdy <php.power.arts@gmail.com>
2017-04-12 07:18:20 +0200

    front end

--- application/models/Mdl_user_verification.php
+++ application/models/Mdl_user_verification.php
@@ -21,21 +21,33 @@ class Mdl_User_verification extends CI_Model {
 			if(!isset($row->passport)){
 				$row->passport = 'http://placehold.it/406x150';
 			}else{
-				$path = base_url('tools/show_passport_upload/'.$row->passport);
+				if($row->passport != ''){
+					$path = base_url('tools/show_passport_upload/'.$row->passport);
+				}else{
+					$path = 0;
+				}
 				$row->passport = $path;
 			}
 
 			if(!isset($row->selfie)){
 				$row->selfie = 'http://placehold.it/406x150';
 			}else{
-				$path = base_url('tools/show_selfie_upload/'.$row->selfie);
+				if($row->selfie != ''){
+					$path = base_url('tools/show_passport_upload/'.$row->selfie);
+				}else{
+					$path = 0;
+				}
 				$row->selfie = $path;
 			}
 
 			if(!isset($row->backcard)){
 				$row->backcard = 'http://placehold.it/406x150';
 			}else{
-				$path = base_url('tools/show_backcard_upload/'.$row->backcard);
+				if($row->backcard != ''){
+					$path = base_url('tools/show_backcard_upload/'.$row->backcard);
+				}else{
+					$path = 0;
+				}
 				$row->backcard = $path;
 			}
 		}else{
@@ -46,44 +58,44 @@ class Mdl_User_verification extends CI_Model {
 
 	public function upload($file='')
 	{
-            $conf['upload_path']   = 'uploads';
-            $conf['allowed_types'] = 'gif|jpg|png|pdf';
-            $conf['overwrite']     = FALSE;
-            $conf['max_size']      = 5000;
-            $conf['max_width']     = 0;
-            $conf['max_height']    = 0;
-            $conf['encrypt_name']  = TRUE;
-            $this->load->library('upload', $conf);
+		$conf['upload_path']   = 'uploads';
+		$conf['allowed_types'] = 'gif|jpg|png|pdf';
+		$conf['overwrite']     = FALSE;
+		$conf['max_size']      = 5000;
+		$conf['max_width']     = 0;
+		$conf['max_height']    = 0;
+		$conf['encrypt_name']  = TRUE;
+		$this->load->library('upload', $conf);
 
-            if(!$this->upload->do_upload($file)){
-                    $this->session->set_flashdata('errors', $this->upload->display_errors('<p>', '</p>'));
-            }else{
-                    $upload_data = $this->upload->data();
-                    $data = [
-                             'verification_trade'=>'unverified',
-                             'user_id' => $this->session->user_id,
-                             $file => $upload_data['raw_name'],
-                             $file.'_path' => 'uploads/'.$upload_data['file_name'],
-                             $file.'_mimetype' => $upload_data['file_type'],
+		if(!$this->upload->do_upload($file)){
+			$this->session->set_flashdata('errors', $this->upload->display_errors('<p>', '</p>'));
+		}else{
+			$upload_data = $this->upload->data();
+			$data = [
+			'verification_trade'=>'unverified',
+			'user_id' => $this->session->user_id,
+			$file => $upload_data['raw_name'],
+			$file.'_path' => 'uploads/'.$upload_data['file_name'],
+			$file.'_mimetype' => $upload_data['file_type'],
                              // $file.'_mimetype' => '',
-                             ];
-                    $this->update($data,$this->session->user_id);
+			];
+			$this->update($data,$this->session->user_id);
 			$this->session->set_flashdata('success','Data Updated');
 		}
 	}
 
 	public function update($data=[],$user_id=0)
 	{
-            if(!$user_id){
-                    $user_id = $this->session->user_id;
-            }
+		if(!$user_id){
+			$user_id = $this->session->user_id;
+		}
 
-            $result = $this->db->get_where($this->table, ['user_id' => $user_id]);
-            if($result->num_rows() > 0){
-                    $this->db->update($this->table, $data, ['user_id' => $user_id]);
-            }else{
-                    $this->db->insert($this->table,$data);
-            }
+		$result = $this->db->get_where($this->table, ['user_id' => $user_id]);
+		if($result->num_rows() > 0){
+			$this->db->update($this->table, $data, ['user_id' => $user_id]);
+		}else{
+			$this->db->insert($this->table,$data);
+		}
 	}
 
 }
