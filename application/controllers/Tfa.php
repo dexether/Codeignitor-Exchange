f65a2b3a3d7849fc1129557d4896ed3958d0e80c
coloman4 <coloman4@outlook.com>
2017-05-13 17:33:16 +0200

    added some comments

--- application/controllers/Tfa.php
+++ application/controllers/Tfa.php
@@ -4,15 +4,20 @@ class TFA extends MY_Controller
 {
     public $data;
 
+    // 1. key is location from where it goes
+    // 2. value is name of function that is to be called if TFA input was good
+    // 3. function should do something and then redirect to other controller
     private $actions = [
         'login' => 'finish_login',
-        'eur_withdraw' => 'finish_withdraw',
+        'currency_withdraw' => 'finish_withdraw',
     ];
+
 
     public function __construct()
     {
         parent::__construct();
 
+        // Checks if user is logged in : if not redirect to index
         if (isset($this->session->user_id) OR isset($this->session->pending_user_id)) {
             
         }else {
@@ -23,13 +28,17 @@ class TFA extends MY_Controller
         $this->data['menu'] = $this->load->view('markets/v_menu', array('uri' => $this->uri->segment(2)), true);
     }
 
-    public function display($route = 'login'){
+
+    // Displays TFA form with route 
+    public function display($route = 'login')
+    {
         $action = base_url() . 'tfa/check_tfa/' . $route;
         $vars = ['action' => $action];
         $this->data['content'] = $this->load->view('user/v_tfa', $vars, true);
         view($this->data, 'site');
     }
     
+
 
     function check_tfa($route)
     {
@@ -45,6 +54,7 @@ class TFA extends MY_Controller
         }
     }
 
+
     private function finish_login()
     {
         $user = $this->mdl_user->get_userdetails($this->session->pending_user_id);
@@ -55,6 +65,12 @@ class TFA extends MY_Controller
     // check tfa against data in db. if correct, set role in sesson and redirect to market.
     // if not correct the error then go to tfa again.
     //  I think you can continue from here
+
+
+
+    //=======================================================================
+    // PRIVATE FUNCTIONS
+    //=======================================================================
 
     private function finish_withdraw()
     {
--- application/controllers/Withdraw.php
+++ application/controllers/Withdraw.php
@@ -3,127 +3,141 @@
 class Withdraw extends MY_Controller
 {
     public $data;
+
+    // see private function check_status()
     private $status;
     private $status_error;
 
+    // allowed currencies, see private function check_currency()
     private $currencies = ['eur', 'nlg', 'gts'];
+
 
     public function __construct()
     {
         parent::__construct();
 
+        //checks if user is logged in
         if (!isset($this->session->user_id)){
             $this->index();
         }
 
         $this->load->model('mdl_withdraw');
-        $this->status_check($this->mdl_withdraw->tfa_bank_status());
+        $this->check_status($this->mdl_withdraw->tfa_bank_status());
     }
 
+
+    // 1. Checks is amount ok, if yes sends is to TFA
+    // 2. If currency is not set returns 404, see private function check_currency()
     public function currency_withdraw($currency = '') 
     {
-    	if (!$this->status) {
-    		$data['error'] = $this->status_error;
-    		$data['menu'] = $this->data['menu'];
-        	$data['content'] = $this->load->view('funds/v_withdraw_error', $data, true);
-        	return $this->load->view('template/v_main_template', $data);
-    	}
+		if (!$this->status) {
+			$data['error'] = $this->status_error;
+			$data['menu'] = $this->data['menu'];
+	    	$data['content'] = $this->load->view('funds/v_withdraw_error', $data, true);
+	    	return $this->load->view('template/v_main_template', $data);
+		}
 
-    	$currency_info = $this->check_currency($currency);
+		$currency_info = $this->check_currency($currency);
 
-        $this->form_validation->set_rules('amount', 'Amount', 'trim|required|numeric');
+	    $this->form_validation->set_rules('amount', 'Amount', 'trim|required|numeric');
 
-        if ($this->form_validation->run() == true) {
-            
-            $amount = abs($this->input->post('amount'));
-            
-            if ((float)$amount <= (float)$currency_info['balance'] && $amount !== 0) {
-                
-                $this->session->pending_curr = [ 'type'=>$currency_info['name'] ,'amount'=>$amount ];
-                redirect('/tfa/display/eur_withdraw');
-                return;
-            }
+	    if ($this->form_validation->run() == true) {
+	        
+	        $amount = abs($this->input->post('amount'));
+	        
+	        if ((float)$amount <= (float)$currency_info['balance'] && $amount !== 0) {
+	            
+	            $this->session->pending_curr = [ 'type'=>$currency_info['name'] ,'amount'=>$amount ];
+	            redirect('/tfa/display/currency_withdraw');
+	            return;
+	        }
 
-            $data['alert'] = '<p class="alert alert-danger">Amount not valid.</p>';
+	        $data['alert'] = '<p class="alert alert-danger">Amount not valid.</p>';
 
-        } else {
-            $data['alert'] = validation_errors('<p class="alert alert-danger">', '</p>');
-        }
+	    } else {
+	        $data['alert'] = validation_errors('<p class="alert alert-danger">', '</p>');
+	    }
 
-        $data['currency'] = $currency_info;
-        $data['menu'] = $this->load->view('markets/v_menu', array('uri'=>$this->uri->segment(2)), true);
-        $data['content'] = $this->load->view('funds/v_eur_withdraw.php', $data, true);
-        return $this->load->view('template/v_main_template', $data);
+	    $data['currency'] = $currency_info;
+	    $data['menu'] = $this->load->view('markets/v_menu', array('uri'=>$this->uri->segment(2)), true);
+	    $data['content'] = $this->load->view('funds/v_eur_withdraw.php', $data, true);
+	    return $this->load->view('template/v_main_template', $data);
    	}
 
 
-	    public function accept_withdraw()
-	    {
-	        //var_dump($this->session->withdraw_conf);
+   	// Checks if some session data was set, returns 404 if not
+    public function accept_withdraw()
+    {
+        if(!isset($this->session->withdraw_conf)) {
+            show_404();
+            return;
+        }
 
-	        if(!isset($this->session->withdraw_conf)) {
-	            show_404();
-	            return;
-	        }
+        if($this->session->withdraw_conf !== true) {
+            show_404();
+            return;
+        }
 
-	        if($this->session->withdraw_conf !== true) {
-	            show_404();
-	            return;
-	        }
-
-	        $this->load->model('mdl_withdraw');
-	        $vars = $this->mdl_withdraw->withdraw_record();
-	        $this->mdl_withdraw->common_mail($vars);        
-	    }
-
-
-	    public function confirm_withdraw ($transaction) 
-	    {
-	        echo 'confirm<br>';
-	        echo $transaction;
-	    }
+        $this->load->model('mdl_withdraw');
+        $vars = $this->mdl_withdraw->withdraw_record();
+        $this->mdl_withdraw->common_mail($vars);        
+    }
 
 
-	    public function cancel_withdraw ($transaction) 
-	    {
-	        echo 'cancel<br>';
-	        echo $transaction;
-	    }
 
-	    private function status_check ($is) 
-	    {
-	    	if (!$is['tfa_enabled']) {
-	    		$this->status = FALSE;
-	    		$this->status_error = 'Sorry, you can not withdraw. TFA must be enabled in settings menu.';
-	    		return;
-	    	}
+    public function confirm_withdraw ($transaction) 
+    {
+        echo 'confirm<br>';
+        echo $transaction;
+    }
 
-	    	if (!$is['bank_verified']) {
-	    		$this->status = FALSE;
-	    		$this->status_error = 'Sorry, you can not withdraw. Bank details must be verified.';
-	    		return;
-	    	}
 
-	    	$this->status = TRUE;
-	    }
+    public function cancel_withdraw ($transaction) 
+    {
+        echo 'cancel<br>';
+        echo $transaction;
+    }
 
-	    private function check_currency($currency) 
-	    {
-	    	$currency = trim($currency);
-	    	if (!in_array($currency, $this->currencies)) {
-	    		show_404();
-	    		die();
-	    		return;
-	    	}
 
-	    	$currency = strtoupper($currency);
-	    	$this->load->model('mdl_balance');
-        	$balance = $this->mdl_balance->fetch_user_balance_by_id($this->session->user_id, $currency);
-        	$currency_info = [ 'name'=>$currency ,'balance'=>number_format($balance, 2)];
-        	return $currency_info;
-	    }
 
-	    private function index() {
-	        redirect('/');
-	    }
+    //=======================================================================
+    // PRIVATE FUNCTIONS
+    //=======================================================================
+    
+    private function check_status ($is) 
+    {
+    	if (!$is['tfa_enabled']) {
+    		$this->status = FALSE;
+    		$this->status_error = 'Sorry, you can not withdraw. TFA must be enabled in settings menu.';
+    		return;
+    	}
+
+    	if (!$is['bank_verified']) {
+    		$this->status = FALSE;
+    		$this->status_error = 'Sorry, you can not withdraw. Bank details must be verified.';
+    		return;
+    	}
+
+    	$this->status = TRUE;
+    }
+
+    private function check_currency($currency) 
+    {
+    	$currency = trim($currency);
+    	if (!in_array($currency, $this->currencies)) {
+    		show_404();
+    		die();
+    		return;
+    	}
+
+    	$currency = strtoupper($currency);
+    	$this->load->model('mdl_balance');
+    	$balance = $this->mdl_balance->fetch_user_balance_by_id($this->session->user_id, $currency);
+    	$currency_info = [ 'name'=>$currency ,'balance'=>number_format($balance, 2)];
+    	return $currency_info;
+    }
+
+    private function index() {
+        redirect('/');
+    }
 }
