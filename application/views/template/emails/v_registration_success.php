fab5883d831d5bba4a43128f2a1cdb7c8050cba2
rogier <rogier@guldentrader.com>
2017-06-08 14:31:31 +0200

    mail, config

--- application/views/template/emails/v_registration_success.php
+++ application/views/template/emails/v_registration_success.php
@@ -6,7 +6,7 @@
 
       <p style="margin: 0pt 20px; padding: 15px 0px; text-align: left; color: rgb(84, 84, 84); font-family: Helvetica,Arial,sans-serif; font-size: 14px;">Thank you for registering at ##COMPANYNAME##</p>
 
-      <p style="margin: 0pt 20px; padding: 15px 0px; text-align: left; color: rgb(84, 84, 84); font-family: Helvetica,Arial,sans-serif; font-size: 14px;">Your account is awaiting for approval. Please <a href="##LINK##">click here</a> to activate your account</p>
+      <p style="margin: 0pt 20px; padding: 15px 0px; text-align: left; color: rgb(84, 84, 84); font-family: Helvetica,Arial,sans-serif; font-size: 14px;">Your account is awaiting activatioen. Please <a href="##LINK##">click here</a> to activate your account</p>
 
       <p style="margin: 0pt 20px; padding: 0px; text-align: left; color: rgb(84, 84, 84); font-family: Helvetica,Arial,sans-serif; font-size: 14px;">Best Regards,</p>
 
